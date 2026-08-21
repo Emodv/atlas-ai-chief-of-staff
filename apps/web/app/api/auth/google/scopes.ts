3bc35e624ca54bc831991e4ac9a27f14ae2c0711
Banoo.Marketing <164055432+Emodv@users.noreply.github.com>
2026-08-20 23:14:02 -0400

    feat: define one-click Google read-only scopes

---
+++ apps/web/app/api/auth/google/scopes.ts
@@ -0,0 +1,13 @@
+export const ATLAS_GOOGLE_READONLY_SCOPES = [
+  "openid",
+  "email",
+  "profile",
+  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/calendar.readonly",
+  "https://www.googleapis.com/auth/contacts.readonly",
+  "https://www.googleapis.com/auth/drive.readonly",
+  "https://www.googleapis.com/auth/documents.readonly",
+  "https://www.googleapis.com/auth/spreadsheets.readonly",
+] as const;
+
+export const ATLAS_GOOGLE_READONLY_SCOPE_STRING = ATLAS_GOOGLE_READONLY_SCOPES.join(" ");
